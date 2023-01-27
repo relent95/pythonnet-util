97fb8f5bae4a79abdc1d2aef85fc9c4d5ac39d09
Shin-myoung-serp <relent95@ahope.co.kr>
2023-01-27 19:55:08 +0900

    Initial commit

---
+++ dotnet/PythonNetUtil/Converter.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace PythonNetUtil;
+
+public class Converter
+{
+	public static unsafe IntPtr PointerToIntPtr(Pointer p)
+	{
+		return (IntPtr)Pointer.Unbox(p);
+	}
+}
